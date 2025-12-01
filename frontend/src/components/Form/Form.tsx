ef59a10888418a5c77882d98775b272c972ffadd
Dexter Du <wxdu64@gmail.com>
2025-12-01 19:29:29 +0800

    feat: Add frontend login flow and shared context

---
+++ frontend/src/components/Form/Form.tsx
@@ -0,0 +1,42 @@
+import type React from "react";
+import "./Form.css";
+import { Button } from "../Button/Button";
+
+interface FormProps {
+  buttonText: string;
+  error: boolean;
+  errorMessage: string;
+  action: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+  height?: string;
+  width?: string;
+}
+
+export const Form: React.FC<FormProps> = ({
+  buttonText,
+  error,
+  errorMessage,
+  action,
+  children,
+  height = "fit-content",
+  width = "fit-content",
+}) => {
+  return (
+    <form
+      className={`column employee-management-form ${
+        error
+          ? "employee-management-form-border-error"
+          : "employee-management-form-border"
+      }`}
+      style={{ height, width }}
+    >
+      {error && (
+        <p className="employee-management-form-error-message">{errorMessage}</p>
+      )}
+      {children}
+      <Button type="primary" handleClick={action}>
+        {buttonText}
+      </Button>
+    </form>
+  );
+};
---
+++ frontend/src/services/GlobalContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useState } from "react";
+import type { Employee } from "../types";
+import type { AuthenticationServiceType } from "./AuthenticationService";
+import useAxios from "./useAxios";
+import useAuthenticationService from "./AuthenticationService";
+
+export type GlobalContextType = {
+  employee: Employee | undefined;
+  employees: Employee[];
+  authenticationService: AuthenticationServiceType;
+};
+
+export type GlobalContextReducers = {
+  updateEmployee: (employee: Employee) => void;
+  updateEmployees: (employees: Employee[]) => void;
+};
+
+const GlobalContext = createContext<GlobalContextType | null>(null);
+
+const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
+
+function GlobalContextProvider(props: { children: React.ReactNode }) {
+  const axiosRequest = useAxios();
+  const [employee, setEmployee] = useState<Employee | undefined>();
+  const [employees, setEmployees] = useState<Employee[]>([]);
+
+  const updateEmployee = (employee: Employee) => {
+    setEmployee(employee);
+  };
+
+  const updateEmployees = (employees: Employee[]) => {
+    setEmployees(employees);
+  };
+
+  const reducers: GlobalContextReducers = {
+    updateEmployee,
+    updateEmployees,
+  };
+
+  const authenticationService = useAuthenticationService(
+    axiosRequest,
+    reducers
+  );
+
+  return (
+    <GlobalContext.Provider
+      value={{ employee, employees, authenticationService }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+}
+
+export { useGlobalContext as default, GlobalContextProvider };
